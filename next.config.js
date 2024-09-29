9a5d0de0c3158a3b5c8dcfdfbc403031f093f630
Pasindu Sandaruwan Silva <146716709+pasindu-silva@users.noreply.github.com>
2024-09-29 11:15:14 +0530

    Update next.config.js - update the base paths

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
+// next.config.js
 module.exports = {
-  basePath: "/portfolio-nextjs",
+  images: {
+    unoptimized: true, // Disable server-side image optimization
+  },
+  // If deploying to a subdirectory, add basePath:
+  basePath: '/portfolio-nextjs', // Replace 'your-repo-name' with the actual repo name if needed
+  assetPrefix: '/portfolio-nextjs/', // Same here
 };
