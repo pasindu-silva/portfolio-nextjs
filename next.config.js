7fc99313bb560baeb87a820017d37f60e30cd3d2
pasindu-silva <146716709+pasindu-silva@users.noreply.github.com>
2024-09-29 11:56:17 +0530

    comment next.config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
-// next.config.js
-module.exports = {
-  images: {
-    unoptimized: true, // Disable server-side image optimization
-  },
-  // If deploying to a subdirectory, add basePath:
-  basePath: '/portfolio-nextjs', // Replace 'your-repo-name' with the actual repo name if needed
-  assetPrefix: '/portfolio-nextjs/', // Same here
-};
+// // next.config.js
+// module.exports = {
+//   images: {
+//     unoptimized: true, // Disable server-side image optimization
+//   },
+//   // If deploying to a subdirectory, add basePath:
+//   basePath: '/portfolio-nextjs', // Replace 'your-repo-name' with the actual repo name if needed
+//   assetPrefix: '/portfolio-nextjs/', // Same here
+// };
